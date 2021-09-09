3feb35e69f4b2e436ea9b50a6e69ec693edc6384
共产主义接班人 <503407184@qq.com>
2021-09-09 13:30:13 +0800

    Add files via upload

--- check accel.py
+++ check accel.py
@@ -1,8 +1,10 @@
 
+import os, re
 import subprocess
 
+
+# https://docs.microsoft.com/en-us/windows/win32/wmisdk/wmic
 def run_wmic_value(key, value):
-    # https://docs.microsoft.com/en-us/windows/win32/wmisdk/wmic
     try:
         return \
             subprocess.check_output(
@@ -46,11 +48,7 @@ def support_vt():
     return "TRUE" == values[1].upper()
 
 
-
-# https://docs.microsoft.com/zh-cn/xamarin/android/get-started/installation/android-emulator/hardware-acceleration
-
-def check_accel():
-    
+def check_vt():
     # wmic os get DataExecutionPrevention_Available
     # wmic cpu get SecondLevelAddressTranslationExtensions
 	# wmic cpu get VMMonitorModeExtensions
@@ -70,6 +68,70 @@ def check_accel():
         and dep and slat and vmmme
 
 
+
+
+
+# https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/sc-query
+def run_sc_query_run(svr):
+    try:
+        arr_values = \
+            re.findall(
+                r"\w+",
+                subprocess.check_output(
+                    ["sc", "query", svr]
+                ).decode()
+            )
+        return "4" == arr_values[ arr_values.index("STATE") + 1]
+    except:
+        raise subprocess.CalledProcessError("execution error")
+
+
+def get_cpu_vendor():
+    if ("GenuineIntel" in os.environ["PROCESSOR_IDENTIFIER"]):
+        return "Intel".lower()
+    if ("AuthenticAMD" in os.environ["PROCESSOR_IDENTIFIER"]):
+        return "AMD".lower()
+    return "unknown"
+
+
+
+# https://developer.android.com/studio/run/emulator-acceleration#accel-check
+# https://docs.microsoft.com/zh-cn/xamarin/android/get-started/installation/android-emulator/hardware-acceleration
+
+def check_accel():
+    
+    accel_drive_download = {
+        "intel": r"https://developer.android.com/studio/run/emulator-acceleration#accel-check",
+        "amd": r"https://docs.microsoft.com/zh-cn/xamarin/android/get-started/installation/android-emulator/hardware-acceleration",
+    }
+    
+    if ("intel" == get_cpu_vendor()):
+        # https://github.com/intel/haxm/releases/latest
+        
+        if (run_sc_query_run("intelhaxm")):
+            return True
+        #
+        print(
+            "no driver detected!\nplease download from: %s" % accel_drive_download[get_cpu_vendor()]
+        )
+    
+    if ("amd" == get_cpu_vendor()):
+        # https://github.com/google/android-emulator-hypervisor-driver-for-amd-processors/releases/latest
+        
+        if (run_sc_query_run("gvm")):
+            return True
+        #
+        print(
+            "no driver detected!\nplease download from: %s" % accel_drive_download[get_cpu_vendor()]
+        )
+    
+    raise RuntimeError("unknown cpu vendor")
+    
+
+
 print(
-    "is support VT accel: %s" % "yep." if (check_accel()) else "no."
+    "is support VT: %s\nis support accel: %s\n" % (
+        "yep." if (check_vt()) else "no.",
+        "yep." if (check_accel()) else "no."
+    )
 )
